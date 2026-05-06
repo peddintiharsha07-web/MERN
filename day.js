c9025ddd67c87517306ee8a1798c80b17d6edab2
Peddinti Harsha Vardhan <knowledgegainersteam20@gmail.com>
2026-05-07 00:05:27 +0530

    message

---
+++ day.js
@@ -0,0 +1,50 @@
+var chintu = "chintu";
+
+console.log(`Hello my name is ${chintu}`);
+
+var chintu = "dancing"
+console.log(`Hello my name is ${chintu}`);
+
+
+let college = "Aitam"
+
+console.log(college);
+
+
+const gender = "male"
+console.log(gender);
+
+college = "GMRIT";
+console.log(college);
+
+
+var a = "welcom to srikakulam";
+console.log(a);
+
+console.log(a.length);
+
+test = 12 > 19;
+console.log(test);
+
+
+r = 34 < 90 < 567;
+console.log(r);
+
+document.write("my name is p Harshavardhanreddy<br>");
+
+document.write(23456789)
+
+document.write("<h3>welcome to my website</h3>")
+
+alert("welcome to my website")
+
+
+let sample = null;
+console.log(sample);
+
+
+let num1 = 12.23;
+console.log(num1);
+
+let banana = "yellow";
+console.log(banana);
